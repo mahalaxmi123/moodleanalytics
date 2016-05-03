64e013945bf54bf3d052cb8773bf12ee5f985785
samridh <samridh.mathur@ballisticlearning.com>
2016-05-03 15:02:33 +0530

    ajax and module.js checked

--- local/moodleanalytics/ajax.php
+++ local/moodleanalytics/ajax.php
@@ -8,16 +8,12 @@
 
 require_once('../../config.php');
 require_once('lib.php');
-require_once('manualbill/lib.php');
-require_once('billslib.php');
-require_once('credit/lib.php');
 
 global $SESSION;
 $context = context_system::instance();
 $PAGE->set_context($context);
 $PAGE->set_pagelayout('admin');
-$pageparams = array();
-$PAGE->set_url('/local/programmanagement/ajax.php', $pageparams);
+$PAGE->set_url('/local/moodleanalytics/ajax.php');
 
 $functionname = optional_param('fname', '', PARAM_ALPHAEXT); //function name as argument
 
@@ -25,75 +21,12 @@ if ($functionname) {
 
     switch ($functionname) {
 
-        case "get_depts":
-            $organization = optional_param('org', 0, PARAM_INT);
-            $result = get_depts($organization);
+        case "get_course_users":
+            print_object($courseid);
+            $courseid = optional_param('id', 0, PARAM_INT);
+            $result = get_course_users($courseid);
             $result = json_encode($result);
             break;
-        case "get_progs":
-            $organization = optional_param('org', 0, PARAM_INT);
-            $result = get_progs($organization);
-            $result = json_encode($result);
-            break;
-        case "get_feeheads":
-            $feecategory = optional_param('feecatid', 0, PARAM_INT);
-            $result = get_feeheads($feecategory);
-            $result = json_encode($result);
-            break;
-        case "get_cycles":
-            $plans = optional_param('planid', 0, PARAM_INT);
-            $pid = optional_param('pid', 0, PARAM_INT);
-            $result = get_cycles($pid, $plans);
-            $result = json_encode($result);
-            break;
-        case "get_progs_org":
-            $organization = optional_param('org', 0, PARAM_INT);
-            $result = get_progs_org($organization);
-            $result = json_encode($result);
-            break;
-        case "get_plans":
-            $pid = optional_param('pid', 0, PARAM_INT);
-            $result = get_plans($pid);
-            $result = json_encode($result);
-            break;
-//        case "get_cycles_plan":
-//            $plans = optional_param('planid', 0, PARAM_INT);
-//            $pid = optional_param('pid', 0, PARAM_INT);
-//            $result = get_cycles_plan($pid, $plans);
-//            $result = json_encode($result);
-//            break;
-        case "get_bills":
-            $pid = optional_param('pid', 0, PARAM_INT);
-            $result = get_bills($pid);
-            $result = json_encode($result);
-            break;
-        
-        case "get_my_due_bills":
-            $userid = required_param('userid', PARAM_INT);
-            $orgid = required_param('orgid', PARAM_INT);
-            $bills = new fee_bills();
-            $duebills = $bills->get_my_due_bills($userid, array('organization'=>$orgid));
-            if($duebills){
-                $result = json_encode($duebills);
-            }else{
-                $result = false;
-            }            
-            break;
-            
-        case "get_credit_balance":
-            $userid = optional_param('userid', $USER->id, PARAM_INT);
-            $orgid = required_param('orgid', PARAM_INT);
-            
-            $credit = new credit($userid);
-            $mycredit = $credit->get_credit_balance($userid, $orgid);
-            if($mycredit){
-                $result = json_encode($mycredit);
-            }else{
-                $result = false;
-            }    
-            $SESSION->mypayment->creditapplied = TRUE;             
-            break;
-        
         default:
             $result = false;
     }
